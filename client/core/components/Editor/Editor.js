123f98cb9433f0e73c5fa5a0a1a2f3b9cc9b04cc
Jorge Betancur <jbetancur@elespectador.com>
2021-12-14 22:11:12 +0000

    WYSIWYG

--- client/core/components/Editor/Editor.js
+++ client/core/components/Editor/Editor.js
@@ -1,44 +1,59 @@
-import { Button, makeStyles, Modal } from '@material-ui/core';
+import {
+  Badge,
+  Button,
+  Chip,
+  makeStyles,
+  Modal,
+  Typography,
+} from '@material-ui/core';
 import React, { useState } from 'react';
 import { isMobile } from 'react-device-detect';
 import WYSIWYG from './WYSIWYG.component';
+import ClearIcon from '@material-ui/icons/Clear';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
+    width: 700,
+    backgroundColor: theme.palette.primary.dark,
     //border: '2px solid #000',
     borderRadius: 5,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+    left: '0',
+    right: '0',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    top: '15%',
   },
   buttonEditor: {
     width: '90%',
     background: theme.palette.primary.light,
     color: '#fff',
   },
+  chipContainer: {
+    display: 'flex',
+    justifyContent: 'end',
+  },
+  chip: {
+    width: 65,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
 }));
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
 function Editor(props) {
   const classes = useStyles();
   const { values, setValues } = props.values;
-  const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -56,19 +71,26 @@ function Editor(props) {
   };
 
   const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id='simple-modal-title'>Contenido</h2>
+    <div className={classes.paper}>
+      {/* <Typography component="subtitle1">Contenido</Typography> */}
+      <div className={classes.chipContainer}>
+        <Chip className={classes.chip} label={values.content.length} />
+      </div>
       <WYSIWYG values={{ values, setValues }} />
-      <Button variant='contained' onClick={handleClose('accept')}>
-        Aceptar
-      </Button>
-      <Button
-        variant='contained'
-        color='secondary'
-        onClick={handleClose('cancel')}
-      >
-        Limpiar
-      </Button>
+      <div className={classes.buttonContainer}>
+        <Button className={classes.button} variant='contained' onClick={handleClose('accept')}>
+          Aceptar
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          onClick={handleClose('cancel')}
+          startIcon={<ClearIcon />}
+          className={classes.button}
+        >
+          Limpiar
+        </Button>
+      </div>
     </div>
   );
 
--- client/core/components/Editor/WYSIWYG.component.js
+++ client/core/components/Editor/WYSIWYG.component.js
@@ -1,20 +1,35 @@
 import React, { useState, useContext } from 'react';
 import { Editor, EditorState, ContentState } from 'draft-js';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
-    margin: theme.spacing(1),
-    width: '90%',
-    border: '1px solid gray',
-    minHeight: 50,
+    border: '1px transparent solid',
+    padding: '1.5em 2em 2.75em 2em',
+    margin: '1.25em',
+    fontSize: '100 %',
+    letterSpacing: '1.2px',
+    borderRadius: '6px',
+    textAlign: 'left',
+    lineHeight: '1.5em',
+    color: 'black',
+    background: '#fff',
+    fontFamily: 'Roboto',
+    overflowY: 'auto',
+    height: '50vh',
   },
 }));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant='filled' {...props} />;
+}
 
 export default function WYSIWYG(props) {
   const classes = useStyles();
   const { values, setValues } = props.values;
 
+  const [open, setOpen] = useState(false);
   const [editorState, setEditorState] = useState(
     EditorState.createWithContent(ContentState.createFromText(values.content)),
   );
@@ -25,12 +40,44 @@ export default function WYSIWYG(props) {
     editor.current.focus();
   }
 
+  const MAX_LENGTH = 2000;
+
   const handleOnChange = (name) => (editorState) => {
     setEditorState(editorState);
     setValues({
       ...values,
       [name]: editorState.getCurrentContent().getPlainText('\u0001'),
     });
+  };
+
+  const _handleBeforeInput = () => {
+    const currentContent = editorState.getCurrentContent();
+    const currentContentLength = currentContent.getPlainText('').length;
+
+    if (currentContentLength > MAX_LENGTH - 1) {
+      setOpen(true);
+
+      return 'handled';
+    }
+  };
+
+  const _handlePastedText = (pastedText) => {
+    const currentContent = editorState.getCurrentContent();
+    const currentContentLength = currentContent.getPlainText('').length;
+
+    if (currentContentLength + pastedText.length > MAX_LENGTH) {
+      setOpen(true);
+
+      return 'handled';
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
   };
 
   React.useEffect(() => {
@@ -43,7 +90,20 @@ export default function WYSIWYG(props) {
         ref={editor}
         editorState={editorState}
         onChange={handleOnChange('content')}
+        handleBeforeInput={_handleBeforeInput}
+        handlePastedText={_handlePastedText}
+        placeholder={<em>Escribe Algo :)</em>}
       />
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity='warning'>
+          Solo puedes escribir máximo 2000 caracteres
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
